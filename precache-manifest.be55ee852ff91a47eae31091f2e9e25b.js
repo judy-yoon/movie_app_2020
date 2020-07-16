689c21085e512fc38986f4937f06b9dd21a91181
Jisu Yoon <jisuyoon@Jisus-MacBook-Air.local>
2020-07-16 04:39:22 -0400

    Updates

---
+++ precache-manifest.be55ee852ff91a47eae31091f2e9e25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fc91c2284394ef8547d9492e363440",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "74f03a95387fcbfedde0",
+    "url": "/movie_app_2020/static/css/main.dc053a6b.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f03a95387fcbfedde0",
+    "url": "/movie_app_2020/static/js/main.08e90658.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
